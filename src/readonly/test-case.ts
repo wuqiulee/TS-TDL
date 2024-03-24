c0eb792e48065e63024f944ddd804632bfc1f95f
wuqiuli <wutongte@163.com>
2024-03-24 12:44:33 +0800

    feat: add readonly

---
+++ src/readonly/test-case.ts
@@ -0,0 +1,12 @@
+import { MyReadonly } from ".";
+
+type cases = [Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>];
+
+interface Todo1 {
+  title: string;
+  description: string;
+  completed: boolean;
+  meta: {
+    author: string;
+  };
+}
